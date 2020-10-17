2e7f76799988d4dd3d1fe84b8dcf77a08f7631cb
vikas <vikas_puri1886@outlook.com>
2020-10-17 18:43:59 +0100

    air-canvas

---
+++ Untitled Folder/untitled3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 20:04:36 2020
+
+@author: vikaspuri
+"""
+
+import numpy as np
+import cv2
+import matplotlib
+
+print('Be1')
